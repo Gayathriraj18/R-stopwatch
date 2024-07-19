a59db3aa26b8bebb3c84c2c9dd191fb9208e41d6
Gayathri <gayathriraj2004@gmail.com>
2024-07-19 15:36:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import Stopwatch from './components/stopwatch';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app">
+      <Stopwatch />
+    </div>
+  );
+};
+
+export default App;
